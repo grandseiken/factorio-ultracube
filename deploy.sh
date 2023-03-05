e8d2e093418c3acddc406f3594cda6b2ce478298
stu <s+github@stu.scot>
2023-03-05 06:59:40 +0000

    deploy version

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,10 @@ DIR="$(dirname "$(readlink -f "$0")")"
 NAME=$(basename "${DIR}")
 MODS="${HOME}/.factorio/mods"
 REGEX=".*/LICENSE\|.*\.lua\|.*\.json"
+VERSION=$(grep '"version"' "${DIR}/info.json" | grep -o '[0-9.]\+')
+ZIPNAME="${NAME}_${VERSION}.zip"
+
 (cd "${DIR}/.." && \
     rm -rf "${NAME}.zip" &&
-    find "${NAME}" -regex "${REGEX}" | xargs zip "${NAME}.zip" &&
-    mv "${NAME}.zip" "${MODS}")
+    find "${NAME}" -regex "${REGEX}" | xargs zip "${ZIPNAME}" &&
+    mv "${ZIPNAME}" "${MODS}")
