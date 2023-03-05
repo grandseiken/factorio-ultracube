a9ae8cd3a2c781b1d35b6f8612d5a784e11ec5ab
stu <s+github@stu.scot>
2023-03-05 09:31:24 +0000

    remove default spawning

--- control.lua
+++ control.lua
@@ -4,12 +4,13 @@ local function on_init()
   if not remote.interfaces.freeplay then
     return
   end
-  game.print("Hypercube init")
 
   -- Set starting items.
   if remote.interfaces["freeplay"] then
     remote.call("freeplay", "set_disable_crashsite", true)
-    remote.call("freeplay", "set_created_items", {})
+    remote.call("freeplay", "set_created_items", {
+      ["iron-chest"] = 4,
+    })
     remote.call("freeplay", "set_respawn_items", {})
   end
 end
--- data.lua
+++ data.lua
@@ -11,3 +11,37 @@ for _, t in pairs(data.raw.recipe) do
     t.expensive.enabled = false
   end
 end
+
+-- Disable all default autoplacing.
+function remove_autoplace_control(name)
+  data.raw["autoplace-control"][name] = nil
+  for _, t in pairs(data.raw["map-gen-presets"]) do
+    for _, preset in pairs(t) do
+      if preset.basic_settings and preset.basic_settings.autoplace_controls then
+        preset.basic_settings.autoplace_controls[name] = nil
+      end
+    end
+  end
+end
+
+remove_autoplace_control("iron-ore")
+remove_autoplace_control("copper-ore")
+remove_autoplace_control("stone")
+remove_autoplace_control("coal")
+remove_autoplace_control("uranium-ore")
+remove_autoplace_control("crude-oil")
+remove_autoplace_control("enemy-base")
+
+data.raw.resource["iron-ore"].autoplace = nil
+data.raw.resource["copper-ore"].autoplace = nil
+data.raw.resource["stone"].autoplace = nil
+data.raw.resource["coal"].autoplace = nil
+data.raw.resource["uranium-ore"].autoplace = nil
+data.raw.resource["crude-oil"].autoplace = nil
+
+data.raw.turret["small-worm-turret"].autoplace = nil
+data.raw.turret["medium-worm-turret"].autoplace = nil
+data.raw.turret["big-worm-turret"].autoplace = nil
+data.raw.turret["behemoth-worm-turret"].autoplace = nil
+data.raw["unit-spawner"]["biter-spawner"].autoplace = nil
+data.raw["unit-spawner"]["spitter-spawner"].autoplace = nil
