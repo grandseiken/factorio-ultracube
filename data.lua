368014b6602a99881af1304bc5400b795573f424
stu <s+github@stu.scot>
2023-04-06 20:02:26 +0100

    todos

--- data.lua
+++ data.lua
@@ -5,6 +5,7 @@ require("prototypes.scripts.autoplace")
 -- TODO: useless stone/coal/wood/etc from decorations.
 -- TODO: output buffer on rare metals / matter, etc, can it be fixed?
 -- TODO: layered icons look bad when disabled. Bake them.
+-- TODO: upgrade graphics for (particularly) boiler, maybe also pipes/tanks/steam engine?
 
 -- TODO: science 2 techs:
 --       move emotional synthesis to its own tech? +make different and not need dormant cube (or cube at all)
@@ -35,16 +36,19 @@ require("prototypes.scripts.autoplace")
 --       - lab speed + increase times (?)
 
 -- TODO: storylore tooltips!
--- TODO: some cube process that outputs random / multiple products.
--- TODO: ultradense constituent and reassembly. Something with weird ratios. Need to recharge many constituents at once?
--- TODO: something that forces cube to go far away on train (cube powered miner, basically)
--- TODO: upgrade graphics for (particularly) boiler, maybe also pipes/tanks/steam engine?
--- TODO: some sort of weird biter farmer spawning synthesis.
--- TODO: late game forbidden tech. Experiments on cube. Suffering. Regret.
--- TODO: some sort of teleport cube logistics option.
--- TODO: (further on) basic antimatter unit. Basic dark matter unit. Matter annihilation (matter + antimatter) -> energy!
--- TODO: eventually, modules.
--- TODO: some joke with hydroxyl group vs abelian group.
+-- TODO: after science 2, split into 3 (?) different focus trees that interlink & eventually merge together.
+--       - 1: maybe scale-focused
+--       - 2: maybe power / energy / byproducts-focused?
+--       - 3: either - cube logistics puzzles, many interlinked recipes with short time and low amounts or
+--            (if possible) computation via recipes, crypts and locks, decoder via furnaces with randomized hidden changing recipes?
+-- TODO: (2) some cube process that outputs random / multiple products.
+-- TODO: (2/1?) ultradense constituent and reassembly. Something with weird ratios. Need to recharge many constituents at once?
+-- TODO: (1?) something that forces cube to go far away on train (cube powered miner, basically)
+-- TODO: (2?) some sort of weird biter farmer spawning synthesis.
+-- TODO: (3) late game forbidden tech. Experiments on cube. Suffering. Regret.
+-- TODO: (1) some sort of teleport cube logistics option.
+-- TODO: (2) (further on) basic antimatter unit. Basic dark matter unit. Matter annihilation (matter + antimatter) -> energy!
+-- TODO: (1) eventually, modules.
 
 require("prototypes.equipment")
 require("prototypes.fluids")
