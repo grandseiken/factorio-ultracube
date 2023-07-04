1b26a587aea702bf49323b880bbee9a12e328209
stu <s+github@stu.scot>
2023-07-04 19:42:42 +0100

    wip deep c/ore

--- prototypes/items_resources.lua
+++ prototypes/items_resources.lua
@@ -191,4 +191,97 @@ data:extend({
     order = "a[calcium]",
     stack_size = 100,
   },
+
+  {
+    type = "item",
+    name = "cube-deep-core-ore",
+    icon = "__Krastorio2Assets__/icons/items-with-variations/raw-imersite/raw-imersite.png",
+    icon_size = 64,
+    icon_mipmaps = 4,
+    pictures = {
+      {
+        layers = {
+          {
+            size = 64,
+            filename = "__Krastorio2Assets__/icons/items-with-variations/raw-imersite/raw-imersite.png",
+            scale = 0.25,
+            mipmap_count = 4,
+          },
+          {
+            draw_as_light = true,
+            flags = {"light"},
+            blend_mode = "additive",
+            tint = {r = 0.3, g = 0.3, b = 0.3, a = 0.3},
+            size = 64,
+            filename = "__Krastorio2Assets__/icons/items-with-variations/raw-imersite/raw-imersite-light.png",
+            scale = 0.25,
+            mipmap_count = 4,
+          },
+        },
+      },
+      {
+        layers = {
+          {
+            size = 64,
+            filename = "__Krastorio2Assets__/icons/items-with-variations/raw-imersite/raw-imersite-1.png",
+            scale = 0.25,
+            mipmap_count = 4,
+          },
+          {
+            draw_as_light = true,
+            flags = {"light"},
+            blend_mode = "additive",
+            tint = {r = 0.3, g = 0.3, b = 0.3, a = 0.3},
+            size = 64,
+            filename = "__Krastorio2Assets__/icons/items-with-variations/raw-imersite/raw-imersite-1-light.png",
+            scale = 0.25,
+            mipmap_count = 4,
+          },
+        },
+      },
+      {
+        layers = {
+          {
+            size = 64,
+            filename = "__Krastorio2Assets__/icons/items-with-variations/raw-imersite/raw-imersite-2.png",
+            scale = 0.25,
+            mipmap_count = 4,
+          },
+          {
+            draw_as_light = true,
+            flags = {"light"},
+            blend_mode = "additive",
+            tint = {r = 0.3, g = 0.3, b = 0.3, a = 0.3},
+            size = 64,
+            filename = "__Krastorio2Assets__/icons/items-with-variations/raw-imersite/raw-imersite-2-light.png",
+            scale = 0.25,
+            mipmap_count = 4,
+          },
+        },
+      },
+      {
+        layers = {
+          {
+            size = 64,
+            filename = "__Krastorio2Assets__/icons/items-with-variations/raw-imersite/raw-imersite-3.png",
+            scale = 0.25,
+            mipmap_count = 4,
+          },
+          {
+            draw_as_light = true,
+            flags = {"light"},
+            blend_mode = "additive",
+            tint = {r = 0.3, g = 0.3, b = 0.3, a = 0.3},
+            size = 64,
+            filename = "__Krastorio2Assets__/icons/items-with-variations/raw-imersite/raw-imersite-3-light.png",
+            scale = 0.25,
+            mipmap_count = 4,
+          },
+        },
+      },
+    },
+    subgroup = "raw-resource",
+    order = "d[0-deep-core-ore]",
+    stack_size = 200,
+  },
 })
