cb5fa60cf816216aac063832c3e5fcf3d022dd0a
stu <s+github@stu.scot>
2023-08-19 18:08:33 +0100

    simulation b

--- menu_simulations/menu_simulations.lua
+++ menu_simulations/menu_simulations.lua
@@ -4,7 +4,7 @@ data.raw["utility-constants"]["default"].main_menu_simulations = main_menu_simul
 main_menu_simulations.cube_a = {
   checkboard = false,
   save = "__Ultracube__/menu_simulations/cube_a.zip",
-  length = 60 * 120,
+  length = 60 * 90,
   init =
   [[
     local logo = game.surfaces.nauvis.find_entities_filtered{name = "factorio-logo-11tiles", limit = 1}[1]
@@ -12,6 +12,24 @@ main_menu_simulations.cube_a = {
     game.camera_zoom = 1
     game.tick_paused = false
     game.surfaces.nauvis.daytime = 0.5
+  ]],
+  update =
+  [[
+    --
+  ]]
+}
+
+main_menu_simulations.cube_b = {
+  checkboard = false,
+  save = "__Ultracube__/menu_simulations/cube_b.zip",
+  length = 60 * 90,
+  init =
+  [[
+    local logo = game.surfaces.nauvis.find_entities_filtered{name = "factorio-logo-11tiles", limit = 1}[1]
+    game.camera_position = {logo.position.x, logo.position.y+9.75}
+    game.camera_zoom = 1
+    game.tick_paused = false
+    game.surfaces.nauvis.daytime = 0.0
   ]],
   update =
   [[
