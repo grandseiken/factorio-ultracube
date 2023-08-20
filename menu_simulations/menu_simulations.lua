869e6974b86d34ecd2fb2497724006cab29fc8f2
stu <s+github@stu.scot>
2023-08-20 17:09:10 +0100

    sim d

--- menu_simulations/menu_simulations.lua
+++ menu_simulations/menu_simulations.lua
@@ -29,7 +29,25 @@ main_menu_simulations.cube_b = {
     game.camera_position = {logo.position.x, logo.position.y+9.75}
     game.camera_zoom = 1
     game.tick_paused = false
-    game.surfaces.nauvis.daytime = 0.0
+    game.surfaces.nauvis.daytime = 0.5
+  ]],
+  update =
+  [[
+    --
+  ]]
+}
+
+main_menu_simulations.cube_d = {
+  checkboard = false,
+  save = "__Ultracube__/menu_simulations/cube_d.zip",
+  length = 60 * 90,
+  init =
+  [[
+    local logo = game.surfaces.nauvis.find_entities_filtered{name = "factorio-logo-11tiles", limit = 1}[1]
+    game.camera_position = {logo.position.x, logo.position.y+9.75}
+    game.camera_zoom = 1
+    game.tick_paused = false
+    game.surfaces.nauvis.daytime = 0.5
   ]],
   update =
   [[
