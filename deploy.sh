997b637192a591c8664180aded8d63d1e96d89ee
stu <s+github@stu.scot>
2023-12-23 18:46:46 +0000

    deploy changelog

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ fi
 DIR="$(dirname "$(readlink -f "$0")")"
 NAME=$(basename "${DIR}")
 MODS="${FACTORIO_DIR}/mods"
-REGEX=".*/LICENSE$\|.*\.lua$\|.*\.cfg$\|.*\.png$\|.*\.ogg$\|.*\.zip\|.*\.json$\|.*\.md$"
+REGEX=".*/LICENSE$\|.*changelog.txt\|.*\.lua$\|.*\.cfg$\|.*\.png$\|.*\.ogg$\|.*\.zip\|.*\.json$\|.*\.md$"
 MODNAME=$(jq -r .name "${DIR}/info.json")
 VERSION=$(jq -r .version "${DIR}/info.json")
 ZIPNAME="${MODNAME}_${VERSION}.zip"
