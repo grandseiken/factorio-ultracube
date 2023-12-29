d78e97e33d50018becab694bfa0626dbc5d7d701
stu <s+github@stu.scot>
2023-12-29 15:17:25 +0000

    tweaking milestones preset

--- scripts/milestones.lua
+++ scripts/milestones.lua
@@ -6,6 +6,8 @@ local milestones = {
   {type="item",  name="cube-deep-introspection-card",        quantity=1},
   {type="item",  name="cube-synthetic-premonition-card",     quantity=1},
   {type="item",  name="cube-complete-annihilation-card",     quantity=1},
+
+  {type="group", name="Science at scale"},
   {type="item",  name="cube-basic-contemplation-unit",       quantity=1000, next="x10"},
   {type="item",  name="cube-fundamental-comprehension-card", quantity=1000, next="x10"},
   {type="item",  name="cube-abstract-interrogation-card",    quantity=1000, next="x10"},
@@ -15,17 +17,24 @@ local milestones = {
 
   -- TODO: re-enable cube usage stats by consumption once supported in milestones mod.
   -- {type="group", name="The Cube"},
-  -- {type="item",  name="cube-ultradense-utility-cube", quantity=100, next="x10"},
+  -- {type="item",  name="cube-ultradense-utility-cube",        quantity=100, next="x10"},
+  -- {type="item",  name="cube-phantom-ultradense-constituent", quantity=1,   next="x10", hidden=true},
+  -- {type="item",  name="cube-legendary-iron-gear",            quantity=1,   next="x10", hidden=true},
 
-  {type="group", name="Resources"},
-  {type="item",  name="cube-basic-matter-unit",  quantity=10000, next="x10"},
-  {type="item",  name="cube-rare-metals",        quantity=1},
-  {type="fluid", name="cube-gelatinous-tar",     quantity=1},
-  {type="item",  name="cube-spectralite",        quantity=1},
-  {type="item",  name="cube-resplendent-plate",  quantity=1},
+  {type="group", name="Components"},
   {type="item",  name="cube-electronic-circuit", quantity=1},
   {type="item",  name="cube-advanced-circuit",   quantity=1},
-  {type="item",  name="cube-spectral-processor", quantity=1},
+  {type="item",  name="cube-spectral-processor", quantity=1, hidden = true},
+  {type="item",  name="cube-basic-motor-unit",   quantity=1},
+  {type="item",  name="cube-advanced-engine",    quantity=1},
+  {type="item",  name="cube-arcane-drive",       quantity=1, hidden = true},
+
+  {type="group", name="Materials"},
+  {type="item",  name="cube-basic-matter-unit",  quantity=1},
+  {type="item",  name="cube-rare-metals",        quantity=1},
+  {type="fluid", name="cube-gelatinous-tar",     quantity=1},
+  {type="item",  name="cube-spectralite",        quantity=1, hidden = true},
+  {type="item",  name="cube-resplendent-plate",  quantity=1, hidden = true},
 
   {type="group", name="Logistics"},
   {type="item",  name="stack-inserter",               quantity=1},
@@ -39,9 +48,9 @@ local milestones = {
   {type="item",  name="cube-beacon",                  quantity=1},
   {type="item",  name="cube-experimental-teleporter", quantity=1, hidden=true},
   {type="item",  name="cube-forbidden-ziggurat",      quantity=1, hidden=true},
-  {type="item",  name="cube-legendary-iron-gear",     quantity=1, hidden=true},
 
   {type="group", name="Power"},
+  {type="item",  name="cube-boiler",             quantity=1},
   {type="item",  name="cube-stygian-energy-lab", quantity=1},
   {type="item",  name="cube-nuclear-reactor",    quantity=1},
   {type="item",  name="cube-antimatter-reactor", quantity=1},
