d4d39e437b5eac1b6ee21c9ddb497933434afebb
stu <s+github@stu.scot>
2024-01-13 21:57:48 +0000

    ctrl+l

--- prototypes/custom_inputs.lua
+++ prototypes/custom_inputs.lua
@@ -2,7 +2,7 @@ data:extend({
   {
     type = "custom-input",
     name = "cube-toggle-alerts",
-    key_sequence = "CONTROL + A",
+    key_sequence = "CONTROL + L",
     consuming = "game-only",
     action = "lua",
   }
