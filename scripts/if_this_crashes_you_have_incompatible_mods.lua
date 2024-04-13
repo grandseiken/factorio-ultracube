439ba1aef15cb9bb30d7980abbff6a81d2cbbb49
stu <s+github@stu.scot>
2024-04-13 13:38:05 +0100

    nicer crash

--- scripts/if_this_crashes_you_have_incompatible_mods.lua
+++ scripts/if_this_crashes_you_have_incompatible_mods.lua
@@ -1,9 +1,8 @@
 return {
   crash = function(message)
-    game.print("Ultracube error: unexpected state. This is almost certainly because you are using another incompatible mod.")
-    game.print("First, try disabling other mods and see if the problem goes away. If you report this error, you must include your mod list.")
-    game.print("The internal error was: " .. message)
-    local intentional_crash = nil
-    intentional_crash[0] = 0
+    crash_message = "Ultracube error: unexpected state. This is almost certainly because you are using another incompatible mod."
+    crash_message = crash_message .. "\nFirst, try disabling other mods and see if the problem goes away. If you report this error, you must include your mod list."
+    crash_message = crash_message .. "\nThe internal error was: " .. message
+    error(crash_message)
   end
 }
