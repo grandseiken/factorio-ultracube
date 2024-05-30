7f07d0822d923968a5982dfecc29cddb9ea9e3e8
stu <s+github@stu.scot>
2024-05-30 12:15:04 +0100

    format/tweak

--- updates/compatibility/renai_transportation_mystery.lua
+++ updates/compatibility/renai_transportation_mystery.lua
@@ -1,18 +1,13 @@
 require("prototypes.lib.mystery")
 
--- Mystery recipes for throwers
-if (settings.startup["RTThrowersSetting"].value == true) then -- Throwers are enabled
-	-- Mystery recipes for throwers
-	add_mystery_recipe(1, "RTThrower-fast-inserter-Item", "RTThrower-inserter-Item") -- Fast thrower to regular thrower
-	-- thrower variants to base inserters
-	add_mystery_recipe(1, "RTThrower-inserter-Item", "inserter")
-	add_mystery_recipe(1, "RTThrower-long-handed-inserter-Item", "long-handed-inserter")
-	add_mystery_recipe(1, "RTThrower-filter-inserter-Item", "filter-inserter")
-	add_mystery_recipe(1, "RTThrower-stack-inserter-Item", "stack-inserter")
-	add_mystery_recipe(1, "RTThrower-stack-filter-inserter-Item", "stack-filter-inserter")
-
-	-- Modded variants
-	if settings.startup["RTModdedThrowers"].value == true then
-		add_mystery_recipe(1, "RTThrower-cube-extremely-long-inserter-Item", "cube-extremely-long-inserter")
-	end
+if settings.startup["RTThrowersSetting"].value then
+  add_mystery_recipe(1, "RTThrower-fast-inserter-Item", "RTThrower-inserter-Item")
+  add_mystery_recipe(1, "RTThrower-inserter-Item", "inserter")
+  add_mystery_recipe(1, "RTThrower-long-handed-inserter-Item", "long-handed-inserter")
+  add_mystery_recipe(1, "RTThrower-filter-inserter-Item", "filter-inserter")
+  add_mystery_recipe(1, "RTThrower-stack-inserter-Item", "stack-inserter")
+  add_mystery_recipe(1, "RTThrower-stack-filter-inserter-Item", "stack-filter-inserter")
+  if settings.startup["RTModdedThrowers"].value then
+    add_mystery_recipe(1, "RTThrower-cube-extremely-long-inserter-Item", "cube-extremely-long-inserter")
+  end
 end
