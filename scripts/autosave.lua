2195a9994f5be2d848f4d6b2efda34a242914ef3
Bentechy66 <sendbenspam@yahoo.co.uk>
2025-03-25 11:20:45 +0000

    minor code cleanup

--- scripts/autosave.lua
+++ scripts/autosave.lua
@@ -3,15 +3,14 @@ local cube_search = require("__Ultracube__/scripts/cube_search")
 local autosave = {}
 
 -- Perform an autosave the first time a Forbidden Ziggurat starts crafting
-function helvetica_scenario_autosave()
+function helvetica_scenario_autosave(tick)
     if storage.has_autosaved_helvetica then return end
 
     local size, results = cube_search.update(tick)
 
-    if size != 1 then return end
-    if size < 1 then return end
+    if size ~= 1 then return end
 
-    local result = results[i]
+    local result = results[1]
     local entity = result.entity
     if entity and entity.valid then
         if entity.name == "cube-forbidden-ziggurat" and entity.crafting_progress > 0 then
@@ -21,6 +20,8 @@ function helvetica_scenario_autosave()
     end
 end
 
-function autosave.on_tick(_tick)
-    helvetica_scenario_autosave()
+function autosave.on_tick(tick)
+    helvetica_scenario_autosave(tick)
 end
+
+return autosave
