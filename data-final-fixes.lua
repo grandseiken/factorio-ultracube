d80a6bdd16efdf28dbf0fc774f252e35d6483713
Bentechy66 <sendbenspam@yahoo.co.uk>
2025-03-30 18:21:13 +0100

    obliterate rocket capacity

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -9,11 +9,16 @@ end
 data.raw["electric-pole"]["cube-local-turbine-source"].maximum_wire_distance = 0
 data.raw["electric-pole"]["cube-local-turbine-transmitter"].maximum_wire_distance = 0
 
+local constants = data.raw["utility-constants"].default
+constants.default_item_weight=0
+constants.rocket_lift_weight=0
+
 -- Make sure items can be sent automatically in the teleporter.
 for type, _ in pairs(defines.prototypes.item) do
   for _, item in pairs(data.raw[type] or {}) do
     --item.rocket_launch_products = {{type = "item", name = "space-science-pack", amount = 0}}
     item.send_to_orbit_mode = "automated"
+    item.ingredient_to_weight_coefficient = 0
   end
 end
 data.raw.item["cube-residual-tendrils"].rocket_launch_products = {{type = "item", name = "cube-residual-tendrils", amount = 100}}
