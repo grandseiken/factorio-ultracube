325ea44b0e700fb7c0853112652120bc9598239a
Kevin Du <5475211+kevin1055@users.noreply.github.com>
2025-06-04 12:49:46 -0700

    revert mystery

--- updates/compatibility/renai_transportation_mystery.lua
+++ updates/compatibility/renai_transportation_mystery.lua
@@ -3,7 +3,6 @@ require("prototypes.lib.mystery")
 if settings.startup["RTThrowersSetting"].value then
   add_mystery_recipe(1, "RTThrower-fast-inserter-Item", "RTThrower-inserter-Item")
   add_mystery_recipe(1, "RTThrower-inserter-Item", "inserter")
-  add_mystery_recipe(1, "RTThrower-fast-inserter-Item", "inserter")
   add_mystery_recipe(1, "RTThrower-long-handed-inserter-Item", "long-handed-inserter")
   add_mystery_recipe(1, "RTThrower-bulk-inserter-Item", "bulk-inserter")
   if settings.startup["RTModdedThrowers"].value then
