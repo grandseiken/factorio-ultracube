469dc8c039fe0bd70349905b2f2e6d04c528e683
Oliver Tessmer <oliver.tessmer@gmail.com>
2026-07-21 20:38:23 -0400

    fix heavy-turbine graphics

--- prototypes/entities/heavy_turbine.lua
+++ prototypes/entities/heavy_turbine.lua
@@ -47,53 +47,67 @@ data:extend({
       type = "electric",
       usage_priority = "secondary-output"
     },
-    horizontal_animation = {
-      layers = {
+    two_direction_only = true,
+    pictures =
+    {
+      north =
+      {
+        animation =
         {
-          filename = "__base__/graphics/entity/steam-turbine/steam-turbine-H.png",
-          width = 320,
-          height = 245,
-          frame_count = 8,
-          line_length = 4,
-          shift = util.by_pixel(0, -2.75),
-          run_mode = "backward",
-          scale = 0.5,
-        },
-        {
-          filename = "__base__/graphics/entity/steam-turbine/steam-turbine-H-shadow.png",
-          width = 435,
-          height = 150,
-          repeat_count = 8,
-          line_length = 1,
-          draw_as_shadow = true,
-          shift = util.by_pixel(28.5, 18),
-          run_mode = "backward",
-          scale = 0.5,
+          layers =
+          {
+            {
+              filename = "__base__/graphics/entity/steam-turbine/steam-turbine-V.png",
+              width = 217,
+              height = 374,
+              frame_count = 8,
+              line_length = 4,
+              shift = util.by_pixel(4.75, 0.0),
+              run_mode = "backward",
+              scale = 0.5
+            },
+            {
+              filename = "__base__/graphics/entity/steam-turbine/steam-turbine-V-shadow.png",
+              width = 302,
+              height = 260,
+              repeat_count = 8,
+              line_length = 1,
+              draw_as_shadow = true,
+              shift = util.by_pixel(39.5, 24.5),
+              run_mode = "backward",
+              scale = 0.5
+            }
+          }
         }
-      }
-    },
-    vertical_animation = {
-      layers = {
+      },
+      east =
+      {
+        animation =
         {
-          filename = "__base__/graphics/entity/steam-turbine/steam-turbine-V.png",
-          width = 217,
-          height = 374,
-          frame_count = 8,
-          line_length = 4,
-          shift = util.by_pixel(4.75, 0.0),
-          run_mode = "backward",
-          scale = 0.5,
-        },
-        {
-          filename = "__base__/graphics/entity/steam-turbine/steam-turbine-V-shadow.png",
-          width = 302,
-          height = 260,
-          repeat_count = 8,
-          line_length = 1,
-          draw_as_shadow = true,
-          shift = util.by_pixel(39.5, 24.5),
-          run_mode = "backward",
-          scale = 0.5,
+          layers =
+          {
+            {
+              filename = "__base__/graphics/entity/steam-turbine/steam-turbine-H.png",
+              width = 320,
+              height = 245,
+              frame_count = 8,
+              line_length = 4,
+              shift = util.by_pixel(0, -2.75),
+              run_mode = "backward",
+              scale = 0.5
+            },
+            {
+              filename = "__base__/graphics/entity/steam-turbine/steam-turbine-H-shadow.png",
+              width = 435,
+              height = 150,
+              repeat_count = 8,
+              line_length = 1,
+              draw_as_shadow = true,
+              shift = util.by_pixel(28.5, 18),
+              run_mode = "backward",
+              scale = 0.5
+            }
+          }
         }
       }
     },
